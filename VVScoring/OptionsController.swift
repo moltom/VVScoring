c3ea448cab538ce7cce208603ce9c588d0b813dc
CLARK <738570@ehs-s-232-15.district196.org>
2016-12-08 10:31:43 -0600

    Testing conflicts

--- VVScoring/OptionsController.swift
+++ VVScoring/OptionsController.swift
@@ -13,7 +13,7 @@ class OptionsController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Do other things
     }
 
     override func didReceiveMemoryWarning() {
